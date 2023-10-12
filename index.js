8b5b28c06306c297434fc51cd0ba6c7a6494d00e
yRaphaael <raphaelgameplays011@gmail.com>
2023-10-12 01:22:00 -0300

    first commit with nodejs and some additions

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Defina o diretório de arquivos estáticos (HTML, CSS, imagens)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para servir a página HTML
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'))
+})
+
+// Inicie o servidor na porta desejada
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
